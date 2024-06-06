185e914f40a0151339662a3d98195b4b24ff9b28
jcguidicelli <guidicelli.jc@gmail.com>
2024-06-06 17:18:16 +0000

    Game development with TDD UPDATED

--- tests/test_game.py
+++ tests/test_game.py
@@ -18,17 +18,33 @@ class TestGame:
         for letter in grid:
             assert letter in string.ascii_uppercase
 
-    def test_game_is_valid(self):
+    def test_empty_word_is_invalid(self):
         # setup
         new_game = Game()
-
-        # exercise
-        grid = list('HELLORRER')
-        words = ['HELLOZ','HELLO']
-
         # verify
-        for word in words:
-            if word == 'HELLOZ':
-                assert new_game.is_valid(word) == False
-            else:
-                assert new_game.is_valid(word) == True 
+        assert new_game.is_valid('') is False
+
+
+    def test_is_valid(self):
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'EUREKA'
+        # exercice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is True
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
+
+    def test_is_invalid(self):
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'SANDWICH'
+        # exerice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is False
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
